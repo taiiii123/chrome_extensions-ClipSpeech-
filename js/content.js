b89f7a799809a7df75f97f8625153d6c2f4279c7
taiiii123 <taiiii88uqtg@gmail.com>
2022-03-22 14:06:05 +0900

    first commit

---
+++ js/content.js
@@ -0,0 +1,8 @@
+let idInputField = document.getElementById("inputField")
+
+let idCopy = document.getElementById("copy");
+let idDelete = document.getElementById("delete");
+let idStart = document.getElementById("start");
+let idStop = document.getElementById("stop");
+
+
